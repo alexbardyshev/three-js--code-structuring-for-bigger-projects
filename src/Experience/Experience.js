e82e1a677356be73e4025a3e55b9fe0a253aac47
Oleksii Bardyshev <oleksii.bardyshev@unilimes.com>
2022-11-07 18:01:25 +0200

    Resources class was created

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -5,6 +5,8 @@ import * as THREE from "three";
 import Camera from "./Camera.js";
 import Renderer from "./Renderer";
 import World from "./World/World.js";
+import Resources from "./Utils/Resources";
+import sources from "./sources.js";
 
 let instance = null;
 
@@ -27,6 +29,7 @@ export default class Experience {
         this.sizes = new Sizes();
         this.time = new Time();
         this.scene = new THREE.Scene()
+        this.resources = new Resources(sources)
         this.camera = new Camera()
         this.renderer = new Renderer()
         this.world = new World()
---
+++ src/Experience/sources.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        name: "environmentMapTexture",
+        type: "cubeTexture",
+        path: [
+            "textures/environmentMaps/px.jpg",
+            "textures/environmentMaps/nx.jpg",
+            "textures/environmentMaps/py.jpg",
+            "textures/environmentMaps/ny.jpg",
+            "textures/environmentMaps/pz.jpg",
+            "textures/environmentMaps/nz.jpg"
+        ]
+    }
+]
